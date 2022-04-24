0667a02f8f895337b2f316cc48346b2447bc4a28
reddysanjeev87 <reddysanjeev87@gmail.com>
2022-04-25 01:56:40 +0600

    sales list page added

---
+++ src/UI/Account.UI/Pages/SalesInvoice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Account.UI.Pages
+{
+    public class SalesInvoiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
